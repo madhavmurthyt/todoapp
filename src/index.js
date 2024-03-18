191f1b546a89254cd34d6c4077627f567afb41a1
mvthotapalli <madhav.thotapalli@skillsoft.com>
2024-03-18 15:15:04 +0530

    basic todo app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './app/App';
-import { Provider } from "react-redux";
-import store from "./app/store";
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App /> 
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
